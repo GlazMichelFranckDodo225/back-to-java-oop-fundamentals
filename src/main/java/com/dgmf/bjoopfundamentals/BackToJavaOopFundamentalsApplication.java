7df67d6ed71735064fa0430c4c63a21b7591fcfd
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-28 07:44:40 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/bjoopfundamentals/BackToJavaOopFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.bjoopfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToJavaOopFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackToJavaOopFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/bjoopfundamentals/BackToJavaOopFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.bjoopfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToJavaOopFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
