cfb261f75aed3fb5e37d1ab427d91ee398054293
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-28 08:24:26 +0200

    Attributes: Access modifier (visibility) ==> "default" (no visibility specified)

--- src/main/java/com/dgmf/model/BankAccount.java
+++ src/main/java/com/dgmf/model/BankAccount.java
@@ -1,11 +1,12 @@
 package com.dgmf.model;
 
+// Modificateurs d'accès (visibilité) : "public", "protected", "package" et "package"
 public class BankAccount {
-    // protected  ==> Attributs accessibles depuis l'intérieur la classe,
-    // depuis les classes dérivées (classes filles)
-    // et depuis les classes se trouvant dans le même package
-    protected String accountId;
-    protected double balance;
-    protected String currency;
-    protected String status;
+    // par défaut (pas de visibilité précisée) ==> correspond à la
+    // visibilité "package" ==> Attributs UNIQUEMENT accessibles depuis
+    // les classes se trouvant dans le même package
+    String accountId;
+    double balance;
+    String currency;
+    String status;
 }
