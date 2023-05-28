d157111508aef208f6a3a843c3eb09f8066621c7
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-28 08:03:08 +0200

    Attributes: Access modifier (visibility) ==> "public"

--- src/main/java/com/dgmf/model/BankAccount.java
+++ src/main/java/com/dgmf/model/BankAccount.java
@@ -1,4 +1,10 @@
 package com.dgmf.model;
 
 public class BankAccount {
+    // public ==> Attributs accessibles depuis n'importe
+    // classes depuis l'extérieur ==> pas de restrictions
+    public String accountId;
+    public double balance;
+    public String currency;
+    public String status;
 }
