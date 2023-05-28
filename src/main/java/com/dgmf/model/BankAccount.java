6bcdddce20a44e565112ce32caf81bbd24451055
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-28 08:13:33 +0200

    Attributes: Access modifier (visibility) ==> "protected"

--- src/main/java/com/dgmf/model/BankAccount.java
+++ src/main/java/com/dgmf/model/BankAccount.java
@@ -1,10 +1,11 @@
 package com.dgmf.model;
 
 public class BankAccount {
-    // private ==> Attributs UNIQUEMENT accessibles depuis
-    // l'intérieur de la classe
-    private String accountId;
-    private double balance;
-    private String currency;
-    private String status;
+    // protected  ==> Attributs accessibles depuis l'intérieur la classe,
+    // depuis les classes dérivées (classes filles)
+    // et depuis les classes se trouvant dans le même package
+    protected String accountId;
+    protected double balance;
+    protected String currency;
+    protected String status;
 }
