352106426f16279f6716779a05e3eac74b6a7fa5
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-28 19:21:41 +0200

    Encapsulation principle - V1 : Displaying the status of the "account1" object using getters, setters and the "static void printAccount(BankAccount account)" method

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -1,5 +1,6 @@
 package com.dgmf;
 
+import com.dgmf.model.BankAccount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		// Création du Handle
+		BankAccount account1 = null;
+		// Création d'un objet et stockage de sa référence
+		account1 = new BankAccount();
+
+		// Modification des attributs de l'objet "account1"
+		account1.setAccountId("123543");
+		account1.setCurrency("$ US");
+		account1.setBalance(7600);
+
+		printAccount(account1);
+
 		SpringApplication.run(Application.class, args);
 	}
 
+	public static void printAccount(BankAccount account) {
+		System.out.println("******** Bank Account **********");
+		System.out.println("ID = " + account.getAccountId());
+		System.out.println("Balance = " + account.getBalance());
+		System.out.println("Status = " + account.getStatus());
+		System.out.println("Currency = " + account.getCurrency());
+		System.out.println("******************************");
+	}
 }
--- src/main/java/com/dgmf/model/BankAccount.java
+++ src/main/java/com/dgmf/model/BankAccount.java
@@ -5,8 +5,45 @@ public class BankAccount {
     // par défaut (pas de visibilité précisée) ==> correspond à la
     // visibilité "package" ==> Attributs UNIQUEMENT accessibles depuis
     // les classes se trouvant dans le même package
-    String accountId;
-    double balance;
-    String currency;
-    String status;
+    private String accountId;
+    private double balance;
+    private String currency;
+    private String status;
+
+    public BankAccount() {
+        status = "CREATED";
+    }
+
+    // Getters et Setters
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
