36366c558802609a21aa347a377301194071e2c6
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-29 06:11:11 +0200

    "equals()" method

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -15,9 +15,30 @@ public class Application {
 		account1.setBalance(7600);
 
 		BankAccount account2 = new BankAccount("£ GB", 1200);
+		// Création d'un objet "account3" en passant les mêmes valeurs que
+		// pour l'objet "account2"
+		BankAccount account3 = new BankAccount("£ GB", 1200);
 
-		printAccount(account1);
+		/*printAccount(account1);
 		printAccount(account2);
+		printAccount(account3);*/
+
+		// Méthode "equals()"
+		System.out.println("******** equals() **********");
+		// Comparaison de "account2" et "account3" ==> Retourne "false"
+		System.out.println("Account2 = Account3 : " + account2.equals(account3));
+		// On leur donne le même id
+		account3.setAccountId(account2.getAccountId());
+		// On les affiche
+		System.out.println(account2);
+		System.out.println(account3);
+		/* Comparaison de "account2" et "account3" ==> Retourne quand même "false"
+		malgré que les 2 objets ont des valeurs d'attributs toutes identiques
+		==> La raison : la méthode "equals()" compare les références (adresses mémoires)
+		des 2 objets et elles sont différentes ici (à cause de l'utilisation
+		de l'opérateur "new" qui crée des objets en leur attribuant des adresses
+		mémoires différentes */
+		System.out.println("Account2 = Account3 : " + account2.equals(account3));
 
 		SpringApplication.run(Application.class, args);
 	}
