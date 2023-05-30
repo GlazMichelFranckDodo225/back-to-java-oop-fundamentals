44aba59fde224552927babbf75e90c224775ce8d
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-30 12:42:11 +0200

    Creating an array and displaying its elements

--- src/main/java/com/dgmf/App2.java
+++ src/main/java/com/dgmf/App2.java
@@ -1,7 +1,24 @@
 package com.dgmf;
 
+import com.dgmf.model.BankAccount;
+
 public class App2 {
     public static void main(String[] args) {
+        // Création d'un tableau de "BankAccount" avec 4 éléments
+        BankAccount[] accounts = new BankAccount[4];
 
+        // Remplissage du tableau "accounts"
+        accounts[0] = new BankAccount("US $", 5400);
+        accounts[1] = new BankAccount("GB £", 1298);
+        accounts[2] = new BankAccount("EUR €", 3200);
+        accounts[3] = new BankAccount("JPY ¥", 2359);
+
+        // Affichage des éléments du tableau "accounts"
+        /*for (int i = 0; i < accounts.length; i++) {
+            System.out.println(accounts[i].toString());
+        }*/
+        for (BankAccount account : accounts) {
+            System.out.println(account.toString());;
+        }
     }
 }
