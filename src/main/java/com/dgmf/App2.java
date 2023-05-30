5f1fb0a26511d1090e794cd9bbe8cdff2877a3fe
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-30 12:53:39 +0200

    ArrayIndexOutOfBoundsException

--- src/main/java/com/dgmf/App2.java
+++ src/main/java/com/dgmf/App2.java
@@ -13,6 +13,11 @@ public class App2 {
         accounts[2] = new BankAccount("EUR €", 3200);
         accounts[3] = new BankAccount("JPY ¥", 2359);
 
+        // ArrayIndexOutOfBoundsException: Index 4 out of bounds for length 4
+        // ==> On cherche à stocker un stocker un 5ième "account" dans un
+        // "accounts" ayant été déclaré comme ayant 4 éléments
+        accounts[4] = new BankAccount("SUI CHF", 7124);
+
         // Affichage des éléments du tableau "accounts"
         /*for (int i = 0; i < accounts.length; i++) {
             System.out.println(accounts[i].toString());
@@ -20,5 +25,7 @@ public class App2 {
         for (BankAccount account : accounts) {
             System.out.println(account.toString());;
         }
+
+        // Affichage d'un
     }
 }
