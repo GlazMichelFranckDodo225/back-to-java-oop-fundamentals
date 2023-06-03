69db38131b7fe069d73a39c6db2228217b3b3f20
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-03 05:58:07 +0200

    Finalizing the "CurrentAccount" class

--- src/main/java/com/dgmf/model/CurrentAccount.java
+++ src/main/java/com/dgmf/model/CurrentAccount.java
@@ -1,4 +1,28 @@
 package com.dgmf.model;
 
-public class CurrentAccount extends BankAccount{
+public class CurrentAccount extends BankAccount {
+    private double overDraft;
+
+    public CurrentAccount() {
+        super();
+        this.overDraft = overDraft;
+    }
+
+    public CurrentAccount(String currency, double initialBalance, double overDraft) {
+        super(currency, initialBalance);
+        this.overDraft = overDraft;
+    }
+
+    public double getOverDraft() {
+        return overDraft;
+    }
+
+    public void setOverDraft(double overDraft) {
+        this.overDraft = overDraft;
+    }
+
+    @Override
+    public String toString() {
+        return "Current Account Overdraft : " + this.overDraft + super.toString();
+    }
 }
